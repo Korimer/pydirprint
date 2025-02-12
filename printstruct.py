5562bfaba66c28147705cf09fca04ac71358729f
Korimer <thekclov@gmail.com>
2025-02-12 12:14:48 -0700

    cleared redundant lines

--- printstruct.py
+++ printstruct.py
@@ -1,12 +1,22 @@
+import builtins
+
 def printstruct(data,whitespace):
-    thereskeys = type(data) == dict
-    for item in data:
-        trueitem = data[item] if thereskeys else item
-        whitespace_format = whitespace + ("{" + item + ": " if thereskeys else "  ")
-        if hasattr(trueitem,"__iter__") and type(trueitem) != str:
-            printstruct(trueitem,whitespace_format)
-        else:
-            print(whitespace_format + str(trueitem))
+    thereskeys = False
+    dattype: type = type(data)
+    
+    if not(hasattr(data,"__iter__")): print(whitespace + data); return # Early return... hrmrmrmr.....
+    limits = {
+        builtins.dict: ("{","}"),
+        builtins.set: ("{","}"),
+        builtins.list: ("{","}"),
+        builtins.set: ("(",")"),
+    }.get(dattype,("<",">"))
+    
+    print(whitespace + limits[0])
+    
+    # Recursive case here
+
+    print(whitespace + limits[1])
 
 printstruct([{"a":1},[{"b":2,"C":[5,{"d":100},7]},{8,9}],0],"")
 
